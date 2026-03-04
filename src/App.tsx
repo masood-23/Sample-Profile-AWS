95aa21dc6254b51638032ea3654bd0ae09b76c21
Mohammad Masood Hassan H A <144266152+masood-23@users.noreply.github.com>
2026-03-04 14:53:11 +0530

    Update name and roll number in App component

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ function App() {
     <>
       <div className="h-screen w-screen flex items-center justify-center p-0 m-0 bg-amber-100/30 overflow-hidden">
         <div className=' h-[30rem] w-[30rem] bg-white rounded-lg shadow-lg shadow-purple-500/30 flex items-center justify-center text-2xl font-bold  flex-col gap-2'>
-          <h1 className='text-purple-500 font-bold text-2xl'>Name</h1>
-          <h5 className='text-gray-500'>rollnumber</h5>
+          <h1 className='text-purple-500 font-bold text-2xl'>Mohammad Masood Hassan </h1>
+          <h5 className='text-gray-500'>rollnumber: 4NI24IS412</h5>
         </div>
       </div>
     </>
